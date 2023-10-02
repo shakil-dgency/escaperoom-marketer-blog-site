da8b1251e2897f9aa96e958a8265c5309690ecd2
shakil <shakil.dgency@gmail.com>
2023-10-02 13:57:38 +0600

    resolve some config for deployment

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "https://fair-poncho-deer.cyclic.cloud" });
 
 export const logIn = (formData) => API.post("/apiuser/signin", formData);
 export const createPost = (newPost) => API.post("/apiposts", newPost);
